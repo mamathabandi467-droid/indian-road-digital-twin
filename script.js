cf2860375bd648cdb743d6ae237636b2f5103784
mamathabandi467-droid <mamathabandi467@gmail.com>
2025-09-13 16:14:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Message in console
+console.log("Website Loaded Successfully ✅");
+
+// Example: run when page loads
+window.onload = function() {
+  console.log("Welcome to Indian Road Digital Twin!");
+};
